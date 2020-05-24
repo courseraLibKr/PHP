a22e3dae73a8c3acffb450ac3dd4e41ec67331ff
courseraLibKr <65415364+courseraLibKr@users.noreply.github.com>
2020-05-24 16:41:24 +0300

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -16,7 +16,7 @@
 	  var byeSpeaker = {};
 	  var speakWord = "Good Bye";
 	  byeSpeaker.name = "Yaakov"; 
-	  byeSpeaker.speak = function () { 
+	  byeSpeaker.speak = function (name) { 
 		  console.log(speakWord + " " + name);
 		}	 	  
 	  window.byeSpeaker = byeSpeaker;
